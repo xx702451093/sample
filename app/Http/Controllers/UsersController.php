6ff66e5f83efdd038d012f0eabbd8fa44436d016
xiaoking <kingofrockme@163.com>
2017-12-05 04:44:07 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,12 +1,34 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create(){
       return view('users.create');
+    }
+    // 个人中心
+    public function show(User $user){
+      return view('users.show', compact('user'));
+    }
+    // 注册
+    public function store(Request $request){
+        // 验证
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required'
+        ]);
+        // 数据创建至数据库
+        $user = User::create([
+           'name' => $request->name,
+           'email' => $request->email,
+           'password' => bcrypt($request->password),
+       ]);
+      //  使用sessoin 赋值方法为flash，键和值
+       session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+      //  重定向至个人中心 并传值
+        return redirect()->route('users.show', [$user]);
     }
 }
